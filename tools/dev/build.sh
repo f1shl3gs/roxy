be52d3ddac05aa496c7398d1a8a3c6caf404ddc9
f1shl3gs <fishlegs.engerman@gmail.com>
2022-09-09 22:01:49 +0800

    use sshd for entrypoint

--- tools/dev/build.sh
+++ tools/dev/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker buildx build -t dev:latest --platform linux/arm64 .
+docker buildx build -t dev:latest --build-arg SSH_PUB_KEY="$(cat ~/.ssh/id_rsa.pub)" --platform linux/arm64 .
